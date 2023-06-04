7d4a089d0bd9d72b74489534880e0fddae36655e
GitHub Action <action@github.com>
2023-06-04 05:35:05 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Dart-check-test"
--- src/main/kotlin/com/github/wtrn/dartchecktest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/wtrn/dartchecktest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.wtrn.dartchecktest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wtrn.dartchecktest.MyBundle
+import com.github.wtrn.dartchecktest.services.MyProjectService
 import javax.swing.JButton
 
 
